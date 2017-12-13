454d1cd3be38d7da6a9299785030dd5612408917
Richard Ogamba <ogambarichard20@gmail.com>
2017-12-13 13:01:44 +0300

    add behaviour to our project

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,11 @@ $(document).ready(function(){
 	});
 });
 var leapYear = function(year) {
-	return false
+	if (year % 4===0){
+		return true;
+	} else {
+		return false;
+	}
 };
 
 	//jQuery user interface logic here.
