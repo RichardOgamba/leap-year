95d525bdfc825c81bced37c9219c212f4af75700
Richard Ogamba <ogambarichard20@gmail.com>
2017-12-13 14:59:37 +0300

    update user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// business logic
 $(document).ready(function(){
 	$("form#leap-year").submit(function(event){
 		event.preventDefault();
@@ -15,4 +16,21 @@ var leapYear = function(year) {
 	}
 };
 
-	//jQuery user interface logic here.
+	// user interface logic
+	$(document).ready(function() {
+		$("form#leap-year").submit(function(event) {
+			event.preventDefault();
+			var year = parseInt($("input#year").val());
+			var result = leapYear(year);
+
+			$(".year").text(year);
+
+			if (!result) {              // same as writing if(result === false)
+				$(".not").text("not");
+			} else {
+				$(".not").text("");
+			}
+			
+			$("#result").show();
+		});
+	});
